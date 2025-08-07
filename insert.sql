27154a028778963bf1dd1ab0fce618649544f504
Gabe Fierro <gtfierro@mines.edu>
2025-08-07 08:31:07 -0600

    add pdf

---
+++ insert.sql
@@ -0,0 +1,26 @@
+-- CREATE TABLE IF NOT EXISTS "papers" (
+-- "type" TEXT,
+--   "title" TEXT,
+--   "pdf" TEXT,
+--   "link" TEXT,
+--   "slides" INTEGER,
+--   "authors" TEXT,
+--   "conference" TEXT,
+--   "location" TEXT,
+--   "month" TEXT,
+--   "year" INTEGER
+-- , award text);
+
+INSERT INTO papers VALUES (
+    "misc",
+    "Systematic Evaluation of Knowledge Graph Repair with Large Language Models",
+    "lin2025systematic.pdf",
+    "https://arxiv.org/abs/2507.22419",
+    null,
+    "Tung-Wei Lin, Gabe Fierro, Han Li, Tianzhen Hong, Pierluigi Nuzzo, Alberto Sangiovanni-Vinentelli",
+    "arXiv",
+    null,
+    "July",
+    2025,
+    null
+);
