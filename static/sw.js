f86913c949766209d4a797b7ff584ef1fed37edf
Gabe Fierro <gtfierro@mines.edu>
2026-01-26 22:16:42 -0700

    ccic demo

---
+++ static/sw.js
@@ -0,0 +1,3 @@
+// StreamSaver.js service worker
+// This is a minimal service worker that imports the StreamSaver mitm script
+self.importScripts('https://cdn.jsdelivr.net/npm/streamsaver@2.0.6/mitm.min.js');
